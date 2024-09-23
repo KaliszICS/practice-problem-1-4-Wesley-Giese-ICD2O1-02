c6f34be8083a9d3afb509596ee8362fa20894484
Wesley-Giese-ICD2O1-02 <Wesley.giese@student.tdsb.on.ca>
2024-09-23 18:24:53 +0000

    i

--- PP1_4.py
+++ PP1_4.py
@@ -1,17 +1,28 @@
-
+'''
+lesson 1.4
+author : Wesley Giese
+date created : sept 19
+last edited sept 19
+'''
 
 def q1():
   #Write Assignment code here
-
+  word1 = input("Input a word: ")
+  print(word1)
 def q2():
   #Write Assignment code here
-
+  word1 = input("Input your first name: ")
+  print(f"Hello {word1}")
 def q3():
   #Write Assignment code here
-
+  word1 = input("Input your first name: ")
+  word2 = input("Input your last name: ")
+  print("{word2} {word1}")
 def q4():
   #Write Assignment code here
-
+  student1 = input(input a student)
+  student2 = input(input another student)
+  print({student1} {student2})
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
 
