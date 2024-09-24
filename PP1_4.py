c8eaafac9ceef648b3ab753f46584fdafeea9850
Wesley-Giese-ICD2O1-02 <Wesley.giese@student.tdsb.on.ca>
2024-09-24 17:34:12 +0000

    h

--- PP1_4.py
+++ PP1_4.py
@@ -7,26 +7,26 @@ last edited sept 23
 
 def q1():
   #Write Assignment code here
-  word1 = input("Input a word: ")
-  print(word1)
+  num = input("Input a word: ")
+  print(num)
 def q2():
   #Write Assignment code here
   word1 = input("Input your first name: ")
   print(f"Hello {word1}")
 def q3():
   #Write Assignment code here
-  word1 = input("Input your first name: ")
-  word2 = input("Input your last name: ")
-  print("{word2} {word1}")
+  name1 = input("Input your first name: ")
+  name2 = input("Input your last name: ")
+  print(f"{name2} {name1}")
 def q4():
   #Write Assignment code here
-  student1 = input(input a student)
-  student2 = input(input another student)
-  print({student1} {student2})
+  student1 = input("Input a student: ")
+  student2 = input("Input another student: ")
+  print(f"Your students are {student1} and {student2}")
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
 
-#q1()
-#q2()
-#q3()
-#q4()
+# q1()
+# q2()
+# q3()
+# q4()
